7b5e550c316163b273c6ffa5f611241958184114
Nargis <nargismahi3@gmail.com>
2022-08-17 12:04:19 +0530

    added project files

---
+++ src/main/java/databaseconnectivity/ConnectionClass.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package databaseconnectivity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nargi
+ */
+public class ConnectionClass {
+    private static ConnectionClass single_instance = null;
+    public Connection connection;
+
+    private ConnectionClass() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://himachaltourism:3306/himachaltourism", "root", "");
+            if (connection != null) {
+                System.out.println("connected");
+
+                String statement = "CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT, "
+                        + "name VARCHAR(255),"
+                        + "email VARCHAR(255), "
+                        + "password VARCHAR(255), "
+                        + "phone VARCHAR(255), "
+                        + "role INT, "
+                        + "PRIMARY KEY(id))";
+                PreparedStatement preparedStatement = connection.prepareStatement(statement);
+                preparedStatement.execute();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionClass.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static ConnectionClass getInstance() {
+        if (single_instance == null) {
+            single_instance = new ConnectionClass();
+        }
+        return single_instance;
+    }
+}
